abcf315ef0d00ae45e76663a1e63cb2e08d27bce
Zeeshan <zeeshanzafar2012@gmail.com>
2021-06-04 20:22:42 +0530

    First commit

---
+++ build/precache-manifest.e9ec338290777da792264c69898c5fea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bebdd92db7eae72b21c470905fdc43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e0e0e7a0f8b2f5fb79",
+    "url": "/static/js/2.ddce2995.chunk.js"
+  },
+  {
+    "revision": "0c373df4432d989386e2",
+    "url": "/static/js/main.aba8346e.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
